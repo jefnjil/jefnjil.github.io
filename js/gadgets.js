cc3eb2a62f82ff916348060df0f5be53ac29b10f
jeffchung0865 <30453157+jeffchung0865@users.noreply.github.com>
2022-04-24 14:50:40 +0800

    Update gadgets.js

--- js/gadgets.js
+++ js/gadgets.js
@@ -5,26 +5,23 @@ var ad='<script async  src="https://pagead2.googlesyndication.com/pagead/js/adsb
 ad += '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-2950260745318461" data-ad-slot="6208933518" data-ad-format="auto"     data-full-width-responsive="true"></ins>';
 ad += '<script> (adsbygoogle = window.adsbygoogle || []).push({}); </script>';
 
-alert(ad);
-
 var adTextDiv = document.getElementById("adText");
 if ( adTextDiv == null ) 
 	document.write( ad );
 else
 	adTextDiv.innerHTML = ad;
-
-if ( adTextDiv != null ) alert('adTextDiv found');
 }
 
+alert(location.href);
 if ( location.href.contains('blog.udn'))
 {
+	// Get Clicky
+	document.write( '<script async src="//static.getclicky.com/26491.js"></script>' );
+
 	// Global site tag (gtag.js) - Google Analytics
 	var ga='<script async src="https://www.googletagmanager.com/gtag/js?id=UA-3719778-1"></script>';
-	ga += '<script>  window.dataLayer = window.dataLayer || [];  function gtag(){dataLayer.push(arguments);}  gtag("js", new Date());  gtag("config", "UA-3719778-1"); </script>';
+	ga += '<script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag("js", new Date()); gtag("config", "UA-3719778-1"); </script>';
 	document.write( ga );
 	
 	alert(ga);
-	
-	// Get Clicky
-	document.write( '<script async src="//static.getclicky.com/26491.js"></script>' );
 }
