e9103669d26b024e04216430a87dccf399ea52b5
jefnjil <102451258+jefnjil@users.noreply.github.com>
2022-06-27 08:30:56 +0800

    疫期便當與餐廳漏網之魚

--- db/blg58.js
+++ db/blg58.js
@@ -15,5 +15,5 @@ switch(docID)
 	case "9134407409937086458": 	reg="zjj";	uss="426473";	break;
 	case "3517111407679093958": 	reg="us16";	uss="568933";	break;
 	case "1004502727395975458": 	reg="sf";	break;
-	case "3455731995264616958" :	reg="snack";	break;
+	case "3455731995264616958" :	reg="snack";	umd="2022.6.27";	uMedia="12751/6407108";	break;
 }
