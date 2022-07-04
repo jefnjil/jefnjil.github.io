1234bb4422a55e82a05fb77cdd91234769ae6928
jefnjil <102451258+jefnjil@users.noreply.github.com>
2022-07-04 13:54:43 +0800

    綠島監獄、燈塔、派吃所

--- db/blg48.js
+++ db/blg48.js
@@ -19,5 +19,5 @@ switch(docID)
 	case "6283256102646869148": 	reg="history";	udn="2013.10.17";	break;
 	case "362182100444322048": 		reg="snack";	break;
 	case "3552191808414872148": 	reg="sf";	break;
-	case "1284292302020565048" :	reg="taitung";	uss="633725";	break;
+	case "1284292302020565048" :	reg="taitung";	uss="633725";	umd="2022.7.4";	uMedia="12749/6427357";	break;
 }
