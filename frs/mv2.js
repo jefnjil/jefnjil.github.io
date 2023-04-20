d7daf0b3ffcda21239369a4b3e817dced6d71a71
jefnjil <102451258+jefnjil@users.noreply.github.com>
2023-04-20 12:55:41 +0800

    超難搞先生

---
+++ frs/mv2.js
@@ -0,0 +1,16 @@
+jnjRefList += '<table border=0 width=100%><tr><td valign=top><p><ul><b>延伸閱讀</b>';
+jnjRef('104339479','jnjmvtv.blogspot.com/2017/07/blog-post','漫漫回家路');
+jnjRef('171306329','jnjmvtv.blogspot.com/2022/01/stillwater','靜水城');
+jnjRef('177229108','jnjmvtv.blogspot.com/2022/10/where-crawdads-sing','沼澤謀殺案');
+jnjRef('171085510','jnjmvtv.blogspot.com/2022/01/holy-motors','花都魅影（Holy Motors）');
+jnjRefList += '</ul></p></td><td valign=top><p><ul>';
+jnjRef('126884760','jnjmvtv.blogspot.com/2019/05/game-of-thrones','權力遊戲（GoT）');
+jnjRef('170912483','jnjmvtv.blogspot.com/2021/12/the-long-call','遙遠的召喚');
+jnjRef('1418282','jnjmvtv.blogspot.com/2007/12/hannibal-lecter','我看Hannibal Lecter');
+jnjRef('6507697','jnjmvtv.blogspot.com/2012/06/hit-miss','小姐與殺手（Hit & Miss）');
+jnjRefList += '</ul></p></td><td valign=top><p><ul>';
+jnjRef('132360359','jnjmvtv.blogspot.com/2020/04/unorthodox','出走布魯克林 (Unorthodox');
+jnjRef('163222485','jnjmvtv.blogspot.com/2021/05/dig-ammonite-mare-of-easttown','古寶、默愛與東城奇案');
+jnjRef('175460531','jnjmvtv.blogspot.com/2022/07/angelyne','看板女王 ─ 安潔琳（Angelyne）');
+jnjRef('178981276','jnjmvtv.blogspot.com/2023/04/blog-post','「超難搞先生」開「經典老爺車」');
+jnjRefList += '</ul></p></td></tr></table>';
