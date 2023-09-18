0b33825934795173f430877aaafdc2945680b0fb
jefnjil <102451258+jefnjil@users.noreply.github.com>
2023-09-18 08:33:46 +0800

    基地（Foundation）」第二季結語

--- db/udn51.js
+++ db/udn51.js
@@ -23,4 +23,5 @@ switch(docID)
 	case "8633051": 	reg="twm";	uss="418590";	break;
 	case "177253651" :	reg="hsinchu";	uss="635047";	umd="2022.10.19";	uMedia="12749/6686944";	break;
 	case "177399151" :	reg="taoyuan";	uss="635049";	ufp="2022.12.6";	umd="2022.11.17";	uMedia="12749/6746060";	break;
+	case "179876951" :	reg="sf2";	break;
 }
