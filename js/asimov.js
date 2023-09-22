01cc2c98a5ad67922efbb02b8a3a1fea87a4ad99
jefnjil <102451258+jefnjil@users.noreply.github.com>
2023-09-22 16:50:19 +0800

    Update asimov.js

--- js/asimov.js
+++ js/asimov.js
@@ -30,13 +30,15 @@ aChapter('2021/02', '百科全書學者', 7 );
 aChapter('2021/03', '市長', 9 );
 aChapter('2021/04', '行商', 6 );
 aChapter('2021/05', '商業鉅子', 18 );
-AsimovList += '</ul></p></td><td valign=top><p><ul>';
 aArticle('2021/06/blog-post', '基地與帝國 序' );
 aChapter('2021/06', '將軍', 3 );
+AsimovList += '</ul></p></td><td valign=top><p><ul>';
 aArticle('2019/12/blog-post', '「基地」的背景' );
 aArticle('2020/12/blog-post', '基地三部曲與我的緣份' );
 aArticle('2021/09/apple-tv-foundation', '「基地（Foundation）」影集初評' );
 aArticle('2021/10/foundation', '「基地 Foundation」影集與「騾」' );
+aArticle('2023/07/foundation', '「基地（Foundation）」第二季初隨筆' );
+aArticle('2023/09/foundation', '「基地（Foundation）」第二季結語' );
 AsimovList += '</ul></p></td></tr></table></center>';
 
 //document.write(AsimovList);
