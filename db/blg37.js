5ff4047ac1beea5cb218a49b3e8cb4e952c5dc6f
jefnjil <102451258+jefnjil@users.noreply.github.com>
2023-12-07 09:42:36 +0800

    唐山大地震

--- db/blg37.js
+++ db/blg37.js
@@ -16,5 +16,5 @@ switch(docID)
 	case "1510838547120865637" :	reg="taoyuan";	uss="635464";	ufp="2022.12.9";	umd="2022.12.2";	uMedia="12749/6800844";	break;
 	case "9080690039591296437" :	reg="miaoli";	uss="637468";	break;
 	case "6848652140725832837" :	reg="crime1";	woman="123164/7461558";	break;
-	case "4627034471888109237" :	reg="mv2";	break;
+	case "4627034471888109237" :	reg="mv2";	woman="123164/7622646";	break;
 }
