986f011b903651236335dcd68b3a99b19e6c42fc
jefnjil <102451258+jefnjil@users.noreply.github.com>
2024-01-31 08:29:39 +0900

    典華會館

---
+++ frs/feast2.js
@@ -0,0 +1,15 @@
+jnjRefList += '<table border=0 width=100%><tr><td valign=top><p><ul><b>延伸閱讀</b>';
+jnjRef('154493306','jnjdining.blogspot.com/2020/12/blog-post','煙波花蓮館');
+jnjRef('178934446','jnjdining.blogspot.com/2023/04/blog-post_17','敦南會館');
+jnjRef('131552608','jnjdining.blogspot.com/2020/01/blog-post_16','真的好海鮮餐廳');
+jnjRefList += '</ul></p></td><td valign=top><p><ul>';
+jnjRef('155976756','jnjdining.blogspot.com/2021/02/blog-post','彭園會館聚餐');
+jnjRef('180210742','jnjdining.blogspot.com/2024/01/blog-post','彭園會館小尾牙');
+jnjRef('178897360','jnjdining.blogspot.com/2023/04/blog-post','三義美食‧吉祥樓餐廳');
+jnjRef('179821121','jnjdining.blogspot.com/2023/09/blog-post','頤宮中餐廳 (三星米其林)');
+jnjRefList += '</ul></p></td><td valign=top><p><ul>';
+jnjRef('156661015','jnjdining.blogspot.com/2021/02/blog-post_22','君品酒店「亮」廳');
+jnjRef('178454300','jnjdining.blogspot.com/2023/02/blog-post','豪鼎中興時尚婚宴會館');
+jnjRef('155448483','jnjdining.blogspot.com/2021/01/blog-post_18','典華會館花田盛事');
+jnjRef('180308223','jnjdining.blogspot.com/2024/01/blog-post_31','典華會館繁華似錦廳');
+jnjRefList += '</ul></p></td></tr></table>';
