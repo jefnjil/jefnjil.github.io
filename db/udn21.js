a8264285bf591fa7a84b4c90352d95a481d53a2f
jefnjil <102451258+jefnjil@users.noreply.github.com>
2024-12-30 08:35:06 +0800

    清境農場

--- db/udn21.js
+++ db/udn21.js
@@ -13,4 +13,5 @@ switch(docID)
 	case "7044521": 	reg="tws";	uss="392377";	break;
 	case "888021": 	reg="book";	ctb="2007.8.1";	break;
 	case "179821121" :	reg="feast1";	woman="123162/7411450";	break;
+	case "181575521" :	reg="nantou1";	break;
 }
--- frs/nantou1.js
+++ frs/nantou1.js
@@ -7,12 +7,13 @@ jnjRef('181177325','jnjfoorprint.blogspot.com/2024/10/blog-post','車埕 ─ 車
 jnjRefList += '</ul></p></td><td valign=top><p><ul><b>日月潭</b>';
 jnjRef('17453515','jnjfoorprint.blogspot.com/2014/09/blog-post_21','日月潭一日快閃');
 jnjRef('3923844','jnjfoorprint.blogspot.com/2010/04/blog-post','山光水色涵碧樓');
+jnjRef('3923836','jnjfoorprint.blogspot.com/2010/04/blog-post_8','日月纜車騰雲霧');
 jnjRef('181197335','jnjdining.blogspot.com/2024/10/blog-post','日月潭伊達邵‧麓司岸餐坊');
 jnjRef('181266909','jnjfoorprint.blogspot.com/2024/11/blog-post','日月潭遊湖一小時');
 jnjRefList += '</ul></p></td><td valign=top><p><ul>';
 jnjRef('17453598','jnjfoorprint.blogspot.com/2014/09/blog-post_20','向山遊客中心');
-jnjRef('3923836','jnjfoorprint.blogspot.com/2010/04/blog-post_8','日月纜車騰雲霧');
 jnjRef('181197336','jnjfoorprint.blogspot.com/2024/10/blog-post_24','日月潭纜車快閃來回');
 jnjRef('17453515','jnjfoorprint.blogspot.com/2014/09/blog-post_21','日月潭自行車道');
 jnjRef('181245048','jnjfoorprint.blogspot.com/2024/10/blog-post_30','向山懸臂式觀景台‧看見拉魯');
+jnjRef('181575521','jnjfoorprint.blogspot.com/2024/12/blog-post','清境農場‧馬術秀');
 jnjRefList += '</ul></p></td></tr></table>';
