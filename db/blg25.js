f5053c192dff20516fabcd34753b3a3f305a2612
jefnjil <102451258+jefnjil@users.noreply.github.com>
2025-09-23 14:09:34 +0800

    金音玻璃丸

--- db/blg25.js
+++ db/blg25.js
@@ -30,4 +30,5 @@ switch(docID)
 	case "5930715201328231025" :	reg="elaine1";	break;
 	case "3741795414767878625" :	reg="economic";	break;
 	case "5919705124733914125" :	reg="news2";	break;
+	case "4449452668531512625" :	reg="news3";	break;
 }
