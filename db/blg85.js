634c5cc0179cb9d94bf0f4f8bb58cdf3884cd428
jefnjil <102451258+jefnjil@users.noreply.github.com>
2026-05-22 08:59:54 +0800

    勝興

--- db/blg85.js
+++ db/blg85.js
@@ -17,4 +17,5 @@ switch(docID)
 	case "5598626887827341185" :	reg="deathpenalty";	break;
 	case "2813644087235606885" :	reg="ma";	break;
 	case "995291444144564185" :	reg="wars";	break;
+	case "5242375668155134285" :	reg="miaoli";	break;
 }
